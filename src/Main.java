79f6f58790627b627a95c8b48831af1180ec6f8d
Alexandru Radu-Todor <allextodor@gmail.com>
2023-12-06 13:08:44 +0200

    day 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import day1.Day1;
-import day2.Day2;
-import day3.Day3;
-import day4.Day4;
-import day5.Day5;
+import day6.Day6;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Day5.part2();
+        Day6.part2();
     }
 }
---
+++ src/day6/Day6.java
@@ -0,0 +1,58 @@
+package day6;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day6 {
+    public static void part1() throws IOException {
+        File file = new File("src/day6/input.txt");
+        Scanner in = new Scanner(file);
+
+        String[] line1Values = in.nextLine().trim().replaceAll(" +", " ").split(" ");
+        String[] line2Values = in.nextLine().trim().replaceAll(" +", " ").split(" ");
+
+        int[] times = Arrays.stream(Arrays.copyOfRange(line1Values, 1, line1Values.length)).mapToInt(Integer::parseInt).toArray();
+        int[] distances = Arrays.stream(Arrays.copyOfRange(line2Values, 1, line2Values.length)).mapToInt(Integer::parseInt).toArray();
+
+        int[] waysToWin = new int[times.length];
+        Arrays.fill(waysToWin, 0);
+        for (int i = 0; i < times.length; i++) {
+            for (int j = 0; j < times[i]; j++) {
+                if ((times[i] - j) * j > distances[i]) {
+                    waysToWin[i] += 1;
+                }
+            }
+        }
+        int ways = 1;
+        for (int way : waysToWin) {
+            ways *= way;
+        }
+
+        System.out.println(ways);
+    }
+
+    public static void part2() throws IOException {
+        File file = new File("src/day6/input.txt");
+        Scanner in = new Scanner(file);
+
+        String line1 = in.nextLine().trim().replaceAll(" +", " ");
+        String line2 = in.nextLine().trim().replaceAll(" +", " ");
+        int spacePos = line1.indexOf(" ");
+        line1 = line1.substring(spacePos + 1).replace(" ", "");
+        spacePos = line2.indexOf(" ");
+        line2 = line2.substring(spacePos + 1).replace(" ", "");
+        long time = Long.parseLong(line1);
+        long distance = Long.parseLong(line2);
+
+        long waysToWin = 0;
+        for (long i = 0; i < time; i++) {
+            if ((time - i) * i > distance) {
+                waysToWin += 1;
+            }
+        }
+
+        System.out.println(waysToWin);
+    }
+}
